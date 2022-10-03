7eab98b9d2def43a627eef3e72cf896093d854f4
Raphael-ESSOB <raphael.essobmussard.ps41819@gmail.com>
2022-10-03 23:34:29 +0400

    Ajout connexion utilisateur

---
+++ login.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * @author Benjamin Carpy <benjamin.carpys@gmail.com>
+ * @copyright Copyright (c) 2002, Benjamin Carpy 
+ * @version 1.0
+ */
+
+include_once("connexionDB.php");
+?>
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="icon.png">
+    <title>Connexion</title>
+</head>
+
+
+<style>
+
+    *{
+        box-sizing: border-box;
+    }
+
+
+    body{
+        width: 100%;
+        height: 100vh;
+        align-items: center;
+    }
+
+    h1, p{
+        text-align: center;
+        margin-bottom: 40px;
+    }
+
+
+    input{
+        display: block;
+        background: rgba(245, 245, 245, 0.699);
+        border: 2px solid #00000000;
+        width: 95%;
+        padding: 10px;
+        margin: 10px auto;
+        border-radius: 5px;
+    }
+
+    label{
+        color: #888;
+        font-size: 18px;
+        padding: 10px;
+    }
+
+    #btn{
+        background: #8a2be2;
+        border: 2px solid #8a2be2;
+    }
+
+    #btn:hover{
+        background: #8a2be2;
+        border: 2px solid #000000;
+    } 
+
+    .containt{
+        height: auto;
+        margin: auto;
+        display: flex;
+        flex-wrap: wrap;
+        padding: 30px;
+        text-align: center;
+        width: 50%;
+        justify-content: center;
+    }
+
+    form{
+        width: 100%;
+    }
+
+</style>
+
+
+
+<body>
+    <div class="containt">
+        <form name="form" action="loginVald.php" method="POST">
+            <img class="mb-4" src="media/icon/icon.ico" alt="" width="72" height="57">
+            <h1 class="h3 mb-3 fw-normal">Identification</h1>
+
+            <div class="form-floating">
+                <label for="floatingInput">Email</label>
+                <input type="email" name="email" class="form-control" id="floatingInput" placeholder="john.michon@gmail.com">
+            </div>
+            <div class="form-floating">
+            <label for="floatingPassword">Mot de passe</label>
+                <input type="password" name="pswd" class="form-control" id="floatingPassword" placeholder="mot de passe">
+            </div>
+
+            <div class="checkboxmb-3">
+                <label><input type="checkbox" value="remember-me">Se souvenir</label>
+            </div>
+
+            <input id="btn" type="submit" name="ConnexionVald" value="Se connecter" />
+            <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+            <p>Vous n'avez pas de compte ? <a href="register.php">S'inscrire</a></p>
+        </form>
+    </div>
+
+</body>
+
+</html>
---
+++ loginVald.php
@@ -0,0 +1,34 @@
+<?php
+// Connexion à la base de données
+include_once("connexionDB.php");
+
+$email = $_POST['email'];
+$pswd = $_POST['pswd'];
+
+
+// Récupération de l'utilisateur lié à l'adresse mail
+$rqt = "SELECT * FROM utilisateur WHERE mail_utilisateur = ?" ;
+
+$cnx = new Connexiondb();
+
+$results = $cnx->executeRequete($rqt , [$email]);
+
+// Si l'utilisateur existe
+if (!empty($results)) {
+    // Si le mot de passe de l'utilisateur correspond à celui envoyé
+    if ($pswd === $results['mdp_utilisateur']){
+        // On démarre une session
+        session_start();
+        // On enregistre les informations de l'utilisateur
+        $_SESSION["connectedUser"] = $results;
+        header('Location: profileUser.php');
+        exit;
+    } else {
+        // Sinon si les mots de passe sont différents (le mdp est erroné) => On renvoi une erreur
+        echo "Email ou mot de passe erroné";
+    }
+    
+} else {
+    // Sinon si l'utilisateur n'existe pas (l'email n'existe pas) => On renvoi une erreur
+    echo "Email ou mot de passe erroné";
+}
